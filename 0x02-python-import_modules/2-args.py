d0752fec8142fa51a034ae68817a63328737f678
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-07-13 11:42:09 -0700

    fix some errors in 2-args.py File

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""prints the number of and the list of its argument"""
-import sys
-n = len(sys.argv)
+if __name__ == "__main__":
 
-if n == 1:
-    print("{} arguments.".format(n - 1))
-elif n == 2:
-    print("{} argument:".format(n - 1))
-else:
-    print("{} arguments:".format(n - 1))
-for i in range(1, n):
-    print("{}: {}".format(i, sys.argv[i]))
+    """prints the number of and the list of its argument"""
+    import sys
+    n = len(sys.argv)
+    if n == 1:
+        print("{} arguments.".format(n - 1))
+    elif n == 2:
+        print("{} argument:".format(n - 1))
+    else:
+        print("{} arguments:".format(n - 1))
+    for i in range(1, n):
+        print("{}: {}".format(i, sys.argv[i]))

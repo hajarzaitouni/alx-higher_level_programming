9783aa402a47e88be3523aedee5cb7d7ba5228a5
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-07-17 18:50:17 -0700

    add 8-multiple_returns.py File

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """calculates the length of a string and its first character"""
+    if len(sentence) == 0:
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])

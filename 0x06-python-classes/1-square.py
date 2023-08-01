511db70f1e55f7f088ea5c4171eda49f9b43d3f4
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-08-01 16:16:19 -0700

    add 1-square.py File

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """define a square class"""
+
+    def __init__(self, size):
+        """The __init__ method initialize in this task
+        the size value of the square
+
+        Args:
+            size (int): the size of square
+        """
+        self.__size = size

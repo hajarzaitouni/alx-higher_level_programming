012e9a6db97675fa37601732dbfe2f627e469ac4
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-08-02 17:26:05 -0700

    add 101-safe_function.py File

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    """executes a function safely"""
+    try:
+        return fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None

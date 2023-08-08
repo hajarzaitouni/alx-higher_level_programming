e377601bb6bf4e84a6f69bb6d4f53e6660a1d43e
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-08-08 09:40:13 -0700

    add some doc to the text_indentation module

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Defines a module that prints a text.
+"""
+
+
 def text_indentation(text):
     """prints a text with 2 new lines,
     after each of these characters: '.', '?' and ':'

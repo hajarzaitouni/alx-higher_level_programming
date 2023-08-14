808e473fb01c3c207231cfc17d1f313864cd149e
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-08-14 10:45:53 -0700

    Implement an inherited class checking function

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!usr/bin/python3
+"""
+Defines an inherited class checking function
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Checks if an object is an inherited (directly or indirectly)
+    instance of a class
+
+    Args:
+        obj (object): the object to be checked.
+        a_class (any): the type of the object.
+
+    Returns: True or False
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False

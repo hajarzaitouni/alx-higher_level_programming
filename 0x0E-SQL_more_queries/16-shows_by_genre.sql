f7b575956ed284a6b1dc7a17fcb1455c8c58ab85
hajar zaitouni <hajarzaitouni88@gmail.com>
2023-09-20 18:43:10 -0700

    update task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,6 +4,6 @@ SELECT tv_s.title, g.name
 FROM tv_shows AS tv_s
 LEFT JOIN tv_show_genres AS tv_g
 ON tv_s.id = tv_g.show_id
-LEFT JOIN tv_genres as g
+LEFT JOIN tv_genres AS g
 ON tv_g.genre_id = g.id
 ORDER BY tv_s.title, g.name;
